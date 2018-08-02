c3e9e444a04ececed7e0fc734d7be5d99676df44
Jane Zhang <janezhang@Janes-MacBook-Pro.local>
2018-08-02 10:11:47 -0700

    Added debounce for search stock price

--- react-app/src/components/SearchBar.js
+++ react-app/src/components/SearchBar.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
 import { BASE, BATCH, BATCH_QUERY } from '../utilities/data/constants';
 import '../styles/searchBar.scss';
-let timeout = null;
-export default class SearchBar extends React.Component {
+export default class SearchBar extends Component {
+
   constructor(props) {
     super(props);
+    this.state = {
+      timer: null
+    };
     this.searchStocks = this.searchStocks.bind(this);
   }
+
   searchStocks = event => {
-    clearTimeout(timeout);
-    timeout = setTimeout(this.getStockDataAJAX(event), 8000);
+    if (this.state.timer) {
+      clearTimeout(this.state.timer);
+    }
+    this.state.timer = setTimeout(() => {
+      this.getStockDataAJAX(event);
+      this.state.timer = null;
+    }, 1000);
   }
 
   getStockDataAJAX(event) {
-    fetch( BASE + event.target.value + BATCH + BATCH_QUERY)
-    .then(result => {
-      return result.json();
-    }).then(data => {
-      console.info('receiving data stock data===', data);
-    });
+    try{
+      console.info('the request url is ', BASE + event.target.value + BATCH_QUERY);
+      fetch( BASE + event.target.value + BATCH_QUERY)
+      .then(result => {
+        return result.json();
+      }).then(data => {
+        console.info('receiving data stock data===', data);
+      });
+    } catch(error) {
+      console.log('can not find this stock ');
+    }
   }
 
   render() {
--- react-app/src/utilities/data/constants.js
+++ react-app/src/utilities/data/constants.js
@@ -1,4 +1,3 @@
-export const BASE = 'https://api.iextrading.com/1.0/stock/';
-export const BATCH = '/batch?';
-export const BATCH_QUERY = 'types=quote,news,chart&range=1m&last=10';
-export const INIT_REQUEST_URL = BASE + 'market/batch?symbols=aapl,fb,goog&types=quote,news,chart&range=1m&last=5';
+export const BASE = 'https://api.iextrading.com/1.0/stock/market/batch?symbols=';
+export const BATCH_QUERY = '&types=quote';
+export const INIT_REQUEST_URL = 'https://api.iextrading.com/1.0/stock/market/batch?symbols=aapl,fb,goog&types=quote,news,chart&range=1m&last=5';
